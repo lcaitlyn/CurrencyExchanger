9a7ac97d613a46fb7411ff29b8141988d73b5e8f
pc <mrrifatcool@gmail.com>
2022-11-21 21:32:51 +0300

    ередалл

--- target/CurrencyExchanger/WEB-INF/classes/data.sql
+++
@@ -1,6 +0,0 @@
-insert into currencyexchanger.currencies (code, fullname, sign) values ('EUR', 'Euro', '€');
-insert into currencyexchanger.currencies (code, fullname, sign) values ('USD', 'US Dollar', '$');
-insert into currencyexchanger.currencies (code, fullname, sign) values ('RUB', 'Russian Ruble', '₽');
-insert into currencyexchanger.currencies (code, fullname, sign) values ('UAH', 'Hryvnia', '₴');
-insert into currencyexchanger.currencies (code, fullname, sign) values ('KZT', 'Tenge', '₸');
-insert into currencyexchanger.currencies (code, fullname, sign) values ('GBP', 'Pound Sterling', '£');
